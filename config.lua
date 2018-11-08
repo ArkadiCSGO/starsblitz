bee4e34adf78f209c344637353d6cb568a2bc9dc
ArkadiCSGO <arkadioffi@gmail.com>
2018-11-08 14:24:20 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.EnableBlips				= true
+Config.EnableJerryCans			= false
+Config.EnableBuyableJerryCans	= false -- Coming soon, currently useless
+Config.VehicleFailure			= 0 -- At what fuel-percentage should the engine stop functioning properly? (Defualt: 10)
